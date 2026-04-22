92a8746299f7aa72867bde429b5ff219247c16b8
slav1978 <slav1978@gmail.com>
2026-04-22 23:36:10 +0300

    Initial Commit

---
+++ LearnSWIFT/LearnSWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSWIFTApp.swift
+//  LearnSWIFT
+//
+//  Created by Slaveyko Slaveykov on 22.04.26.
+//
+
+import SwiftUI
+
+@main
+struct LearnSWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
